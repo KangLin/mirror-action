7b0d5f990098eca4e2a4b2f04e252a8d6ed8e45b
Kang Lin <kl222@126.com>
2023-03-04 12:57:50 +0800

    Update mirror.yml

--- mirror.sh
+++ mirror.sh
@@ -37,9 +37,9 @@ function mirror {
 
         echo "step${index}: mirror`expr $index - 2` to ${des_rep}"
         echo "git remote add $1_${index} "$des_rep""
-        git remote add $1_${index} "$des_rep";
-        echo "eval git push ${GIT_PUSH_ARGS} $1_${index} "\"refs/remotes/origin/*:refs/heads/*\"""
-        eval git push ${GIT_PUSH_ARGS} $1_${index} "\"refs/remotes/origin/*:refs/heads/*\""
+        git remote add $1_`expr $index - 2` "$des_rep";
+        echo "eval git push ${GIT_PUSH_ARGS} $1_`expr $index - 2` "\"refs/remotes/origin/*:refs/heads/*\"""
+        eval git push ${GIT_PUSH_ARGS} $1_`expr $index - 2` "\"refs/remotes/origin/*:refs/heads/*\""
 
     done
 
@@ -47,14 +47,25 @@ function mirror {
 
 # "remote_name soruce_repository destination_repository1 destination_repository2 ..."
 rep=("RabbitRemoteControl https://github.com/KangLin/RabbitRemoteControl.git ssh://kl222@git.code.sf.net/p/rabbitremotecontrol/code git@bitbucket.org:kl222/rabbitremotecontrol.git")
-rep+=("SerialPortAssistant https://github.com/KangLin/SerialPortAssistant.git ssh://kl222@git.code.sf.net/p/serialportassistant/code git@gitlab.com:kl222/SerialPortAssistant.git")
+rep+=("SerialPortAssistant https://github.com/KangLin/SerialPortAssistant.git ssh://kl222@git.code.sf.net/p/serialportassistant/code git@gitlab.com:kl222/SerialPortAssistant.git git@bitbucket.org:kl222/serialportassistant.git")
 rep+=("RabbitIm https://github.com/KangLin/RabbitIm.git ssh://kl222@git.code.sf.net/p/rabbitim/code")
-rep+=("Calendar https://github.com/KangLin/Calendar.git ssh://kl222@git.code.sf.net/p/rabbitcalendar/code git@gitlab.com:kl222/Calendar.git")
-rep+=("LunarCalendar https://github.com/KangLin/LunarCalendar.git ssh://kl222@git.code.sf.net/p/lunarcalendar/code git@gitlab.com:kl222/LunarCalendar.git")
-rep+=("chinesechesscontrol https://github.com/KangLin/ChineseChessControl.git ssh://kl222@git.code.sf.net/p/chinesechesscontrol/code git@gitlab.com:kl222/ChineseChessControl.git")
+rep+=("LunarCalendar https://github.com/KangLin/LunarCalendar.git ssh://kl222@git.code.sf.net/p/lunarcalendar/code git@gitlab.com:kl222/LunarCalendar.git git@bitbucket.org:kl222/lunarcalendar.git")
+rep+=("Calendar https://github.com/KangLin/Calendar.git ssh://kl222@git.code.sf.net/p/rabbitcalendar/code git@gitlab.com:kl222/Calendar.git ")
+rep+=("chinesechesscontrol https://github.com/KangLin/ChineseChessControl.git ssh://kl222@git.code.sf.net/p/chinesechesscontrol/code git@gitlab.com:kl222/ChineseChessControl.git git@bitbucket.org:kl222/chinesechesscontrol.git")
 rep+=("RabbitProxyServer https://github.com/KangLin/RabbitProxyServer.git ssh://kl222@git.code.sf.net/p/rabbitproxyserver/code")
-rep+=("RabbitCommon https://github.com/KangLin/RabbitCommon.git ssh://kl222@git.code.sf.net/p/rabbitcommon/code")
+rep+=("RabbitCommon https://github.com/KangLin/RabbitCommon.git ssh://kl222@git.code.sf.net/p/rabbitcommon/code git@bitbucket.org:kl222/rabbitcommon.git")
+rep+=("FaceRecongnizer https://github.com/KangLin/FaceRecongnizer.git git@gitlab.com:kl222/FaceRecognizer.git ssh://kl222@git.code.sf.net/p/facerecognizer/code")
 rep+=("Documents https://github.com/KangLin/Documents.git git@gitlab.com:kl222/Documents.git")
+rep+=("TransformCoordinate https://github.com/KangLin/TransformCoordinate.git git@gitlab.com:kl222/TransformCoordinate.git ssh://kl222@git.code.sf.net/p/transformcoordinate/code")
+rep+=("pre2006 https://github.com/KangLin/pre2006 git@gitlab.com:kl222/pre2006.git")
+
+#rep+=("RabbitRemoteControl https://github.com/KangLin/RabbitRemoteControl.git git@invent.kde.org:kanglin/RabbitRemoteControl.git")
+#rep+=("SerialPortAssistant https://github.com/KangLin/SerialPortAssistant.git git@invent.kde.org:kanglin/SerialPortAssistant.git")
+#rep+=("LunarCalendar https://github.com/KangLin/LunarCalendar.git git@invent.kde.org:kanglin/LunarCalendar.git")
+#rep+=("Calendar https://github.com/KangLin/Calendar.git git@invent.kde.org:kanglin/Calendar.git")
+#rep+=("FaceRecongnizer https://github.com/KangLin/FaceRecongnizer.git git@invent.kde.org:kanglin/facerecognizer.git")
+#rep+=("RabbitCommon https://github.com/KangLin/RabbitCommon.git git@invent.kde.org:kanglin/rabbitcommon.git")
+#rep+=("chinesechesscontrol https://github.com/KangLin/ChineseChessControl.git git@invent.kde.org:kanglin/ChineseChessControl.git")
 
 for i in "${rep[@]}";
 do
